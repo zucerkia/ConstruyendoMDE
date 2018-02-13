2b19924f0553c49142be78db0d09407c3f14cce8
zucerkia <julian.sucerquia@upb.edu.co>
2018-02-13 16:40:32 -0500

    funcionamiento del movimiento en todos los modulos

--- js/juego.js
+++ js/juego.js
@@ -12,19 +12,27 @@ var height = canvas.getAttribute("height");
 
 */
 
+var contador_torre = 0;
+var torre;
+var main;
+var escenario;
+
+var pos='350px'
+var neg='-'+pos;
+var direccion= pos;
+var numBloques=2;
+var distBase=30;
+var velocidad = 2000;
+var altura=800;
 
 $(document).ready(function(){
 
-  var torre = $('.torre');
-  var escenario = $('.escenario');
-  //var idTorre =$('#torre');
 
-    var pos='350px'
-    var neg='-'+pos;
-    var direccion= pos;
-    var numBloques=2;
-    var distBase=30;
-    var velocidad = 2000;
+  escenario = $('.escenario');
+  main = $('main');
+  //var idTorre =$('#torre');
+  torre = $('#torre');
+
 
 
     //agregarBloque(distBase*numBloques)
@@ -32,78 +40,67 @@ $(document).ready(function(){
 
 
 
-    $("main").click(function(){
+    main.click(function(){
 
-        clearInterval(animacion);
+      if(numBloques<=9){
+
+
+
+        //clearInterval(animacion);
         torre.stop();
 
-        $("#torre").removeClass('.torre');
+        //$("#torre").removeClass('.torre');
 
         agregarBloque(distBase*numBloques,numBloques);
         numBloques++;
         console.log(numBloques);
         //setInterval(animar,1000);
 
+      }
+      else{
 
+          parseInt(altura)+40;
+
+          main.css("height",altura+"px");
+          //main.scrollTop();
+          //clearInterval(animacion);
+          torre.stop();
+
+          //$("#torre").removeClass('.torre');
+
+          agregarBloque(distBase*numBloques,numBloques);
+          numBloques++;
+          console.log(numBloques);
+
+
+
+      }
     });
 
 
       function animar(){
+        if(torre){
+          if(direccion==pos){
+            torre.animate({left: direccion}, velocidad);
+            direccion=neg;
+          }
+          else{
+            torre.animate({left: direccion}, velocidad);
+            direccion=pos;
 
-        if(direccion==pos){
-          torre.animate({left: direccion}, velocidad);
-          direccion=neg;
-         }
-         else{
-          torre.animate({left: direccion}, velocidad);
-      direccion=pos;
-
-         }
+          }
+        }
       }
 
       function agregarBloque(distBase,numBloques){
         var distancia = distBase+130;
-        $("main").append("<div class='torrePos torre' id='torre' style='bottom:"+distancia+"px'><img src='/img/modulo"+numBloques+".png'></div>");
-      }
-/*
-    requestAnimationFrame(cycle);
+        var temp = "<div class='torrePos torre' id='torre_"+(++contador_torre)+"'";
+        temp += " style='bottom:"+distancia+"px'><img src='/img/modulo"+numBloques+".png'></div>";
 
-    function cycle(time) {
-
-      var multPhase = Number(document.querySelector("select").value);
-      var angPhase = 2 * Math.PI;
-      for (var i = 1; i <= nBalls; i++) {
-        var xMAS = xArmonic(t, R - r, freq, angPhase * i * multPhase, width / 2);
-        var pos = rotate(xMAS, angPhase * i / 2, [width / 2, (height-80) / 2]);
-        drawBall(pos[0], pos[1], r, i);
+        console.log(contador_torre);
+        $("main").append(temp);
+        torre = $("#torre_"+contador_torre);
       }
 
-      torre.animate({right: 350+pos+'px'});
-
-      // incrementa el tiempo en 1/60 seg, incrementa el contador y ejecuta la animacion
-      t += 1 / 60;
-      counter++;
-      requestAnimationFrame(cycle);
-    }
-
-    function drawBall(x, y, r, i) {
-      ctx.beginPath();
-      ctx.fillStyle = "black";
-      ctx.arc(x, y, r, 0, 2 * Math.PI);
-      ctx.fill();
-      ctx.font = "24px Arial";
-      ctx.fillStyle = "black";
-      ctx.fillText(i, x - 13, y + 7);
-    }
-
-    function xArmonic(t, Amp, freq, angPhase) {
-      return Amp * Math.cos(2 * Math.PI * freq * t + angPhase);
-    }
-
-    function rotate(r, ang, center) {
-      return [r * Math.cos(ang) + center[0], r * Math.sin(ang) + center[1]];
-    }
-
-*/
 
 });
