928991af2a974a8c0f516276437c3d5aa80456d4
zucerkia <julian.sucerquia@upb.edu.co>
2018-02-21 14:20:20 -0500

    version inicial funcional del juego en phaser

--- js/index.js
+++ js/index.js
@@ -20,3 +20,49 @@ $(document).ready(function(){
 
 });
 */
+var game;
+var gameOptions={
+
+	gameWidth:480,
+	gameHeight:800
+}
+
+game = new Phaser.Game(gameOptions.gameWidth, gameOptions.gameHeight, Phaser.AUTO, 'game');
+/*
+window.onload() = function(){
+
+
+  game.state.add("Playgame", playGame);
+  game.state.start("playgame");
+
+}
+*/
+var playGame = {
+
+	preload:function() {
+
+	  game.load.image('fondo', 'img/inicio/fondo.jpg ');
+
+	},
+
+	create:function() {
+
+	  game.add.sprite(0, 0,'fondo');
+	},
+
+	 update:function() {
+
+	}
+
+}
+	game.state.add('Playgame', playGame);
+	game.state.start('Playgame');
+
+
+/*
+Playgame.prototype = {
+
+	
+}
+
+*/
