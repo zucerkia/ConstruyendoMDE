b59e3d16b9d34d1c3a6ce622d73bc5b6995c9a0e
zucerkia <julian.sucerquia@upb.edu.co>
2018-02-22 13:05:40 -0500

    correccion del spritesheet play y adicion de carpeta def

--- js/index.js
+++ js/index.js
@@ -20,41 +20,77 @@ $(document).ready(function(){
 
 });
 */
-var game;
 var gameOptions={
 
 	gameWidth:480,
 	gameHeight:800
 }
-
-game = new Phaser.Game(gameOptions.gameWidth, gameOptions.gameHeight, Phaser.AUTO, 'game');
-/*
-window.onload() = function(){
+var game = new Phaser.Game(gameOptions.gameWidth, gameOptions.gameHeight, Phaser.AUTO, 'game');
 
 
-  game.state.add("Playgame", playGame);
-  game.state.start("playgame");
-
-}
-*/
 var playGame = {
 
 	preload:function() {
 
-	  game.load.image('fondo', 'img/inicio/fondo.jpg ');
+		this.load.image('fondo', 'img/inicio/fondo.jpg ');
+		this.load.image('imgInicio', 'img/inicio/pantallaDeInicio.png');
+		this.load.spritesheet('btnPlay','img/inicio/play.png',150,111);
+		
 
 	},
 
 	create:function() {
 
-	  game.add.sprite(0, 0,'fondo');
+		this.game.add.sprite(0, 0,'fondo');
+		this.game.add.sprite(0,0,'imgInicio');
+		this.btnPlay = this.game.add.button(game.world.centerX,game.world.centerY,'btnPlay',map ,this,1,1,2);
+		this.btnPlay.anchor.setTo(-0.20,-1.5);
+		
+
+		
 	},
+
 
 	 update:function() {
 
 	}
 
 }
+
+var map ={
+
+	
+	preload:function() {
+
+		this.load.image('fondo', 'img/inicio/fondo.jpg ');
+		this.load.image('imgInicio', 'img/inicio/pantallaDeInicio.png');
+		this.load.spritesheet('btnPlay','img/inicio/play.png',150,111);
+		
+
+	},
+
+	create:function() {
+
+		this.game.add.sprite(0, 0,'fondo');
+		this.game.add.sprite(0,0,'imgInicio');
+		this.btnPlay = this.game.add.button(game.world.centerX,game.world.centerY,'btnPlay',map ,this,1,1,2);
+		this.btnPlay.anchor.setTo(-0.20,-1.5);
+		
+
+		
+	},
+
+
+	 update:function() {
+
+	}
+
+}
+
+function goToMap() {
+    game.state.start('GamePlay');
+}
+
 	game.state.add('Playgame', playGame);
 	game.state.start('Playgame');
 
